d110d23ba15876ceaa60f1741d78a1734df729e9
Naomi Navkal <navkal@hotmail.com>
2016-08-10 21:48:09 -0400

    Acclaim prototype

--- acclaim.php
+++ acclaim.php
@@ -3,8 +3,16 @@
 {
   .padTopWide
   {
-    padding-top: 50px;
+    padding-top: 55px;
   }
+}
+.padBottom
+{
+  padding-bottom: 20px;
+}
+.padTop
+{
+  padding-top: 30px;
 }
 </style>
 
@@ -14,26 +22,26 @@
       <img src="Loft.jpg" class="img-responsive" style="width:100%" alt="Loft Opera">
     </div>
   </div>
-  <div style="padding-top:30px">
+  <div class="padTop">
     <div class="row">
       <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-        <img src="NYT.jpg" class="img-responsive" style="width:100%; padding-bottom:20px" alt="New York Times">
+        <img src="NYT.jpg" class="img-responsive padBottom" style="width:100%" alt="New York Times">
       </div>
       <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-        <p class="padTopWide" style="padding-bottom:20px" >
+        <p class="padTopWide padBottom" >
           "The tenor Nikhil Navkal was splendid as Gennaro, conveying the confused ardor of a young man who is driven by passions he can't quite identify. He sang with a vibrant, lean tenor and an unerring sense of line and phrasing."
         </p>
       </div>
     </div>
   </div>
   <hr>
-  <div style="padding-top:30px">
+  <div class="padTop">
     <div class="row">
       <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-        <img src="Observer.jpg" class="img-responsive" style="width:100%; padding-bottom:20px" alt="New York Observer">
+        <img src="Observer.jpg" class="img-responsive padBottom" style="width:100%" alt="New York Observer">
       </div>
       <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-        <p class="padTopWide" style="padding-bottom:20px" >
+        <p class="padTopWide padBottom" >
           "How a tiny company like LoftOpera finds such quality voices is beyond me, but I'm not complaining: better just to praise Nikhil Navkal for his silky tenor and pouty bad-boy looks as Gennaro"
         </p>
       </div>

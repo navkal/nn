4d97233701a8546a5c24cbb1d37b392ecfca383c
Naomi Navkal <navkal@hotmail.com>
2016-08-10 22:08:53 -0400

    Acclaim prototype

--- acclaim.php
+++ acclaim.php
@@ -1,9 +1,9 @@
 <style>
-@media( min-width: 992px )
+@media( min-width: 768px )
 {
   .padTopWide
   {
-    padding-top: 55px;
+    padding-top: 45px;
   }
 }
 .padBottom
@@ -18,16 +18,20 @@
 
 <div class="container">
   <div class="row">
-    <div class="col-xs-10 col-sm-8 col-md-6 col-lg-6">
-      <img src="Loft.jpg" class="img-responsive" style="width:100%" alt="Loft Opera">
+    <div class="col-xs-11 col-sm-8 col-md-6 col-lg-6">
+      <a href="http://www.loftopera.com/" target="_blank" >
+        <img src="Loft.jpg" class="img-responsive" style="width:100%" alt="Loft Opera">
+      </a>
     </div>
   </div>
   <div class="padTop">
     <div class="row">
-      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-        <img src="NYT.jpg" class="img-responsive padBottom" style="width:100%" alt="New York Times">
+      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+      <a href="http://www.nytimes.com/2015/04/07/arts/music/review-loftoperas-lucrezia-borgia-with-flying-bottles-of-beer-and-poison.html?_r=0" target="_blank" >
+        <img src="NYT.jpg" class="img-responsive padBottom" style="width:100%; border:0" alt="New York Times">
+      </a>
       </div>
-      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
         <p class="padTopWide padBottom" >
           "The tenor Nikhil Navkal was splendid as Gennaro, conveying the confused ardor of a young man who is driven by passions he can't quite identify. He sang with a vibrant, lean tenor and an unerring sense of line and phrasing."
         </p>
@@ -37,10 +41,12 @@
   <hr>
   <div class="padTop">
     <div class="row">
-      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-        <img src="Observer.jpg" class="img-responsive padBottom" style="width:100%" alt="New York Observer">
+      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+        <a href="http://observer.com/2015/04/the-met-can-learn-a-beverage-lesson-from-loftopera/" target="_blank" >
+          <img src="Observer.jpg" class="img-responsive padBottom" style="width:100%; border:0" alt="Observer">
+        </a>
       </div>
-      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
         <p class="padTopWide padBottom" >
           "How a tiny company like LoftOpera finds such quality voices is beyond me, but I'm not complaining: better just to praise Nikhil Navkal for his silky tenor and pouty bad-boy looks as Gennaro..."
         </p>

a5630e8d5d6547c1add02b0e7f5fa20841d73e73
Naomi Navkal <navkal@hotmail.com>
2016-08-10 21:42:31 -0400

    Acclaim prototype

--- acclaim.php
+++ acclaim.php
@@ -1,28 +1,39 @@
+<style>
+@media( min-width: 992px )
+{
+  .padTopWide
+  {
+    padding-top: 50px;
+  }
+}
+</style>
+
 <div class="container">
   <div class="row">
     <div class="col-xs-10 col-sm-8 col-md-6 col-lg-6">
       <img src="Loft.jpg" class="img-responsive" style="width:100%" alt="Loft Opera">
     </div>
   </div>
-  <div style="padding-top:50px">
+  <div style="padding-top:30px">
     <div class="row">
       <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-        <img src="NYT.jpg" class="img-responsive" style="width:100%" alt="Nikhil Navkal">
+        <img src="NYT.jpg" class="img-responsive" style="width:100%; padding-bottom:20px" alt="New York Times">
       </div>
       <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-        <p style="padding-top:40px; padding-bottom:30px" >
+        <p class="padTopWide" style="padding-bottom:20px" >
           "The tenor Nikhil Navkal was splendid as Gennaro, conveying the confused ardor of a young man who is driven by passions he can't quite identify. He sang with a vibrant, lean tenor and an unerring sense of line and phrasing."
         </p>
       </div>
     </div>
   </div>
-  <div style="padding-top:50px">
+  <hr>
+  <div style="padding-top:30px">
     <div class="row">
       <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-        <img src="Observer.jpg" class="img-responsive" style="width:100%" alt="Nikhil Navkal">
+        <img src="Observer.jpg" class="img-responsive" style="width:100%; padding-bottom:20px" alt="New York Observer">
       </div>
       <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-        <p style="padding-top:40px; padding-bottom:30px" >
+        <p class="padTopWide" style="padding-bottom:20px" >
           "How a tiny company like LoftOpera finds such quality voices is beyond me, but I'm not complaining: better just to praise Nikhil Navkal for his silky tenor and pouty bad-boy looks as Gennaro"
         </p>
       </div>

a248859c1ae88bcf2f351ddf4b257f06d2d79b24
Naomi Navkal <navkal@hotmail.com>
2016-08-10 21:24:50 -0400

    Acclaim prototype

--- acclaim.php
+++ acclaim.php
@@ -1,9 +1,7 @@
 <div class="container">
-  <div style="padding-top:50px">
-    <div class="row">
-      <div class="col-xs-10 col-sm-8 col-md-6 col-lg-6">
-        <img src="Loft.jpg" class="img-responsive" style="width:100%" alt="Loft Opera">
-      </div>
+  <div class="row">
+    <div class="col-xs-10 col-sm-8 col-md-6 col-lg-6">
+      <img src="Loft.jpg" class="img-responsive" style="width:100%" alt="Loft Opera">
     </div>
   </div>
   <div style="padding-top:50px">
@@ -12,7 +10,7 @@
         <img src="NYT.jpg" class="img-responsive" style="width:100%" alt="Nikhil Navkal">
       </div>
       <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-        <p style="padding-top:50px; padding-bottom:30px" >
+        <p style="padding-top:40px; padding-bottom:30px" >
           "The tenor Nikhil Navkal was splendid as Gennaro, conveying the confused ardor of a young man who is driven by passions he can't quite identify. He sang with a vibrant, lean tenor and an unerring sense of line and phrasing."
         </p>
       </div>
@@ -24,7 +22,7 @@
         <img src="Observer.jpg" class="img-responsive" style="width:100%" alt="Nikhil Navkal">
       </div>
       <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-        <p style="padding-top:50px; padding-bottom:30px" >
+        <p style="padding-top:40px; padding-bottom:30px" >
           "How a tiny company like LoftOpera finds such quality voices is beyond me, but I'm not complaining: better just to praise Nikhil Navkal for his silky tenor and pouty bad-boy looks as Gennaro"
         </p>
       </div>

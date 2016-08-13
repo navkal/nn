57ab79325b93ddf07c466047a8bc6423d992a7ed
Naomi Navkal <navkal@hotmail.com>
2016-08-12 23:51:25 -0400

    Thumbnails

--- gallery/gallery.php
+++ gallery/gallery.php
@@ -1,10 +1,39 @@
+<style>
+  .image
+  {
+    position:relative;
+    overflow:hidden;
+    padding-bottom:100%;
+  }
+
+  .image img.landscape
+  {
+    position: absolute;
+    max-height: 100%;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+  }
+
+  .image img.portrait
+  {
+    position: absolute;
+    max-width: 100%;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+  }
+</style>
+
 <?php
-  function showImage( $jpgname )
+  function showImage( $jpgname, $imgclass="landscape" )
   {
 ?>
     <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
       <a href="javascript:void(0)" class="thumbnail" >
-        <img src="gallery/images/<?=$jpgname?>.jpg" alt="<?=$jpgname?>.jpg" >
+        <div class="image" >
+          <img src="gallery/images/<?=$jpgname?>.jpg" alt="<?=$jpgname?>.jpg" class="<?=$imgclass?>" >
+        </div>
       </a>
     </div>
 <?php
@@ -20,9 +49,9 @@
       showImage( "4869746_orig" );
       showImage( "970890_orig" );
       showImage( "4217558_orig" );
-      showImage( "2320357_orig" );
+      showImage( "2320357_orig", "portrait" );
       showImage( "5294250_orig" );
-      showImage( "7196577_orig" );
+      showImage( "7196577_orig", "portrait" );
     ?>
   </div>
 </div>

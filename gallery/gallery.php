4c5f76ef04166cc407b380ef79f6429203bfe22f
Naomi Navkal <navkal@hotmail.com>
2016-09-29 17:59:52 -0400

    Thumbnails and resume

--- gallery/gallery.php
+++ gallery/gallery.php
@@ -89,7 +89,7 @@
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
         <h4 class="modal-title" id="galleryLabel">Gallery</h4>
       </div>
-      <div class="modal-body bg-info">
+      <div class="modal-body">
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>

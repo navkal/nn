8b0490e6513db559f0eae48a940d286b86611663
Naomi Navkal <navkal@hotmail.com>
2016-10-07 10:37:11 -0400

    Gallery

--- contact/contact.php
+++ contact/contact.php
@@ -9,11 +9,19 @@
   else
   {
     $to = "NaomiNavkal@gmail.com";
-    $subject = "Website contact message";
-    $text =  $_POST["firstName"];
-    $email = "your@big.fan";
-    $headers = "From: " . $email . "<NaomiNavkal@gmail.com>" . "\r\n";
+
+    $name = $_POST["firstName"] . " " . $_POST["lastName"];
+    $subject = "Comment by " . $name;
+
+    $text =
+      "Name: " . $name . PHP_EOL . PHP_EOL .
+      "Email: " . $_POST["email"] . PHP_EOL . PHP_EOL .
+      "Comment: " . PHP_EOL . $_POST["comment"] . PHP_EOL . PHP_EOL;
+
+    $headers = "From: " . $_POST["email"] . "<NaomiNavkal@gmail.com>" . "\r\n";
+
     mail( $to, $subject, $text, $headers );
+
     sayThankYou();
   }
 ?>
@@ -31,7 +39,25 @@
         <div class="panel-body">
           <form id="contactForm" role="form" onsubmit="return onSubmitContact();" method="post" enctype="multipart/form-data" >
 
-            <input type="text" name="firstName" required />
+            <div class="form-group">
+              <label for="email">Email Address</label>
+              <input type="email" class="form-control" id="email" name="email" placeholder="Email Address">
+            </div>
+
+            <div class="form-group">
+              <label for="firstName">First Name</label>
+              <input type="text" class="form-control" id="firstName" name="firstName" placeholder="First Name">
+            </div>
+
+            <div class="form-group">
+              <label for="lastName">Last Name</label>
+              <input type="text" class="form-control" id="lastName" name="lastName" placeholder="Last Name">
+            </div>
+
+            <div class="form-group">
+              <label for="comment">Comment</label>
+              <textarea class="form-control" id="comment" name="comment" rows="3"></textarea>
+            </div>
 
             <!-- Form buttons -->
             <div class="row">

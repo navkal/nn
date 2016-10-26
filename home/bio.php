24a1070fbe47c56dae6141f6faa3b0fa03c60f30
Naomi Navkal <navkal@hotmail.com>
2016-10-26 14:57:39 -0400

    Contact common code

--- home/bio.php
+++ home/bio.php
@@ -1,23 +1,33 @@
-<p>
-  American tenor Nikhil Navkal is rapidly establishing himself as one of the leading tenors of his generation.  In 2016, he will join Opera Australia in nine shows at the iconic Sydney Opera House, interpreting the role of Nadir in Bizet’s <i>Les pêcheurs de perles</i>, directed by Michael Gow.
-</p>
+<div class="row">
+  <div class="col-xs-12 col-sm-2 col-md-2 col-lg-3">
+  </div>
+  <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
+    <div class="lato">
+      <p>
+        American tenor Nikhil Navkal is rapidly establishing himself as one of the leading tenors of his generation.  In 2016, he will join Opera Australia in nine shows at the iconic Sydney Opera House, interpreting the role of Nadir in Bizet’s <i>Les pêcheurs de perles</i>, directed by Michael Gow.
+      </p>
 
-<p>
-  In April 2015, he performed with Brooklyn’s LoftOpera, portraying Gennaro in Donizetti’s <i>Lucrezia Borgia</i>.  Other recent performances include scenes from Rossini’s <i>Il Barbiere di Siviglia</i> alongside Joyce DiDonato in a recital to promote LoftOpera, and the role of Dorvil in Rossini's <i>La Scala di Seta</i> with Festival d'Aix-en-Provence in France.  While studying at the Yale School of Music, he interpreted the roles of Don Ramiro in <i>La Cenerentola</i> by Rossini, and Tebaldo in <i>I Capuleti e I Montecchi</i> by Bellini.
-</p>
+      <p>
+        In April 2015, he performed with Brooklyn’s LoftOpera, portraying Gennaro in Donizetti’s <i>Lucrezia Borgia</i>.  Other recent performances include scenes from Rossini’s <i>Il Barbiere di Siviglia</i> alongside Joyce DiDonato in a recital to promote LoftOpera, and the role of Dorvil in Rossini's <i>La Scala di Seta</i> with Festival d'Aix-en-Provence in France.  While studying at the Yale School of Music, he interpreted the roles of Don Ramiro in <i>La Cenerentola</i> by Rossini, and Tebaldo in <i>I Capuleti e I Montecchi</i> by Bellini.
+      </p>
 
-<p>
-  Navkal's concert repertoire includes Handel’s <i>Messiah</i>, Rossini’s <i>Stabat Mater</i>, Dvorák's <i>Stabat Mater</i>, Britten’s <i>Saint Nicolas</i>, and Beethoven’s <i>Symphony No. 9</i>.  He has performed concert works under the batons of Maestros Helmuth Rilling, William Boughton, and Speranza Scappucci.
-</p>
+      <p>
+        Navkal's concert repertoire includes Handel’s <i>Messiah</i>, Rossini’s <i>Stabat Mater</i>, Dvorák's <i>Stabat Mater</i>, Britten’s <i>Saint Nicolas</i>, and Beethoven’s <i>Symphony No. 9</i>.  He has performed concert works under the batons of Maestros Helmuth Rilling, William Boughton, and Speranza Scappucci.
+      </p>
 
-<p>
-  He participated as a Bel Canto Young Artist at the Caramoor Festival, performing First Commissioner in Poulenc’s <i>Dialogues des carmélites</i>, and covering Fernand in Donizetti’s <i>La Favorite</i>.  He also performed as a young artist at Central City Opera.
-</p>
+      <p>
+        He participated as a Bel Canto Young Artist at the Caramoor Festival, performing First Commissioner in Poulenc’s <i>Dialogues des carmélites</i>, and covering Fernand in Donizetti’s <i>La Favorite</i>.  He also performed as a young artist at Central City Opera.
+      </p>
 
-<p>
-  Navkal earned an Encouragement Award in the Metropolitan Opera National Council auditions, New York District.
-</p>
+      <p>
+        Navkal earned an Encouragement Award in the Metropolitan Opera National Council auditions, New York District.
+      </p>
 
-<p>
-  He holds Master of Music degrees from both Yale School of Music and Mannes Conservatory in New York, and a Bachelor of Music degree in vocal performance from Indiana University.
-</p>
+      <p>
+        He holds Master of Music degrees from both Yale School of Music and Mannes Conservatory in New York, and a Bachelor of Music degree in vocal performance from Indiana University.
+      </p>
+    </div>
+  </div>
+  <div class="col-xs-12 col-sm-2 col-md-2 col-lg-3">
+  </div>
+</div>

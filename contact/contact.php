db50fd2ee2fc9a32f67e6137ddbc5209bff56e82
Naomi Navkal <navkal@hotmail.com>
2016-10-30 20:22:37 -0400

    Email icon in footer

--- contact/contact.php
+++ contact/contact.php
@@ -12,7 +12,7 @@
   <?php
     require_once $_SERVER["DOCUMENT_ROOT"]."/../common/contact.php";
     $zemskygreen = "#607765";
-    contact( "h2", "Nikhil&nbsp;Navkal",  "NikhilNavkalContact@gmail.com", "I", "Nikhil", "white", $zemskygreen, "contact/clef.jpg" );
+    contact( "h2", "Nikhil&nbsp;Navkal", "I", "Nikhil", "white", $zemskygreen, "contact/clef.jpg" );
   ?>
 
 </div>

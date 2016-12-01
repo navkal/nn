b5f50890fcda2d965cfa05171147fff35822f909
Naomi Navkal <navkal@hotmail.com>
2016-12-01 18:09:50 -0500

    Refine About and Biography pages

--- home/home.php
+++
@@ -1,8 +0,0 @@
-<img src="home/banner.jpg" class="img-responsive" style="margin:auto; width:100%; max-width:1300px" alt="Nikhil Navkal">
-<br/>
-
-<div class="container">
-  <?php
-    include "bio.php";
-  ?>
-</div>

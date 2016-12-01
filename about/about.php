b853bd21c541580ca54ab028007018bb8ec569d4
Naomi Navkal <navkal@hotmail.com>
2016-12-01 11:34:49 -0500

    Add About and Biography test pages

---
+++ about/about.php
@@ -0,0 +1,46 @@
+<img src="about/banner.jpg" class="img-responsive" style="margin:auto; width:100%; max-width:1300px" alt="Nikhil Navkal">
+<br/>
+
+<div class="container">
+  <div class="row">
+
+    <div class="col-xs-12 col-sm-2 col-md-2 col-lg-3">
+    </div>
+
+    <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
+      <div class="lato">
+        <p>
+          American tenor Nikhil Navkal combines a vibrant singing voice with rare musical intelligence and an authentic stage presence.  His interpretations are a subtle interplay of song, poetry, and drama.
+        </p>
+
+        <p>
+          In the past year, Navkal sang with Opera Australia at the iconic Sydney Opera House, rendering the role of Nadir in Bizet’s <i>Les pêcheurs de perles</i>, directed by Michael Gow.
+        </p>
+
+        <p>
+          In April 2015, he performed with Brooklyn’s LoftOpera, portraying Gennaro in Donizetti’s <i>Lucrezia Borgia</i>.  Other recent performances include scenes from Rossini’s <i>Il Barbiere di Siviglia</i> alongside Joyce DiDonato in a recital to promote LoftOpera, and the role of Dorvil in Rossini's <i>La Scala di Seta</i> with Festival d'Aix-en-Provence in France.  While studying at the Yale School of Music, he interpreted the roles of Don Ramiro in <i>La Cenerentola</i> by Rossini, and Tebaldo in <i>I Capuleti e I Montecchi</i> by Bellini.
+        </p>
+
+        <p>
+          Navkal's concert repertoire includes Handel’s <i>Messiah</i>, Rossini’s <i>Stabat Mater</i>, Dvorák's <i>Stabat Mater</i>, Britten’s <i>Saint Nicolas</i>, and Beethoven’s <i>Symphony No. 9</i>.  He has performed concert works under the batons of Maestros Helmuth Rilling, William Boughton, and Speranza Scappucci.
+        </p>
+
+        <p>
+          He participated as a Bel Canto Young Artist at the Caramoor Festival, performing First Commissioner in Poulenc’s <i>Dialogues des carmélites</i>, and covering Fernand in Donizetti’s <i>La Favorite</i>.  He also performed as a young artist at Central City Opera.
+        </p>
+
+        <p>
+          Navkal earned an Encouragement Award in the Metropolitan Opera National Council auditions, New York District.
+        </p>
+
+        <p>
+          He holds Master of Music degrees from both Yale School of Music and Mannes Conservatory in New York, and a Bachelor of Music degree in vocal performance from Indiana University.
+        </p>
+      </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-2 col-md-2 col-lg-3">
+    </div>
+
+  </div>
+</div>
---
+++ biography/biography.php
@@ -0,0 +1,102 @@
+<!-------------------------------------------------- >
+<img src="biography/banner.jpg" class="img-responsive" style="margin:auto; width:100%; max-width:1300px" alt="Nikhil Navkal">
+<br/>
+<!-------------------------------------------------->
+
+<div class="container">
+  <div class="row">
+
+    <div class="col-xs-12 col-sm-2 col-md-2 col-lg-3">
+    </div>
+
+    <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
+      <div class="lato">
+
+        <h4>
+          Early Childhood
+        </h4>
+
+        <p>
+          I grew up in the suburbs of Boston, Massachusetts, USA, in a multi-ethnic family.
+          My father emigrated from Mumbai, India, and my mother is Jewish-American.
+        </p>
+
+        <p>
+          As a child, I had access to a large collection of classical music recordings, both Indian and Western.
+          I spent long hours listening to many beloved instrumental and vocal masterpieces.
+          Handel’s Messiah was my favorite above all.
+          By singing along with the recording, I memorized the text and began to develop my sense of phrasing and vibrato.
+        </p>
+
+        <h4>
+          School Years
+        </h4>
+
+        <p>
+          At age 7, I began to study the violin; yet singing remained my most innate and heartfelt mode of musical expression.
+          In my early teens, I started voice lessons – first with a local teacher in my home town of Andover, and later at the New England Conservatory Preparatory Division in Boston.
+          In summers, I attended several programs for young poeple, including the Bel Canto Institute in New Paltz, NY, and Boston University Tanglewood Institute (BUTI).
+          I also became fascinated by the art of violinmaking, and spent a summer as an apprentice violinmaker, constructing a violin from scratch with my own hands.
+        </p>
+
+        <h4>
+          College
+        </h4>
+
+        <p>
+          During my college years, I majored in vocal performance at the Indiana University Jacobs School of Music in Bloomington, IN.
+          At IU, I performed comprimario roles in student productions of La Traviata and Lucia di Lammermoor.
+          I pursued additional summertime training at the Ezio Pinza Council for American Singers of Opera (EPCASO) in Oderzo, Italy.
+        </p>
+
+        <p>
+          As a lifelong vegetarian, I enjoyed shopping at the Bloomington Farmers' Market, where I befriended a Mennonite farmer.
+          This farmer welcomed me into his home as one of the family, and on his farm as an occasional farm hand.
+          I became an avid cyclist, commuting to school and the farm by bicycle, and once even cycling through the Italian Alps, from the Milan airport to the EPCASO program in Oderzo.
+          I also played Ultimate Frisbee with the Indiana University team and had my first exposure to yoga, which I continue to practice today.
+          In my senior year of college, I competed in the annual Indiana University Little 500 bicycle race.
+        </p>
+
+        <h4>
+          Graduate School
+        </h4>
+
+        <p>
+          After college, I moved to New York City, enrolling in graduate school at Mannes College.
+          After earning a Master’s degree from Mannes, I enrolled in the opera program at the Yale School of Music, where I completed a second Master’s degree.
+          At Yale, I performed my first lead roles in full opera productions: Don Ramiro in <i>La Cenerentola</i>, and Tebaldo in <i>I Capuleti e I Montecchi</i>.
+          During my graduate school years, I spent summers at various programs for young artists and emerging professionals, including International Vocal Arts Institute (IVAI), Central City Opera, and Festival d'Aix-en-Provence in France, where I portrayed Dorvil in <i>La Scala di Seta</i>.
+        </p>
+
+        <h4>
+          Emerging Artist
+        </h4>
+
+        <p>
+          Returning to New York City after graduation, I joined former Mannes colleagues in LoftOpera, a young, innovative troupe based in Brooklyn.
+          There, in April 2015, I performed the role of Gennaro in <i>Lucrezia Borgia</i>, for which I earned high praise in the New York Times and several other publications.
+        </p>
+
+        <p>
+          One year later, I had the privilege to perform with Opera Australia, in the Sydney Opera House, as Nadir in Bizet’s <i>Les pêcheurs de perles</i>.
+        </p>
+
+        <p>
+          WHEN WAS THE MET COMPETITION?
+        </p>
+
+        <p>
+          After attending my performance with LoftOpera, Bruce Zemsky and Alan Green of
+          <a href="http://zemskygreenartists.com/contact-us-2/" target="_blank" >Zemsky Green Artists Management</a>
+          generously offered me a place on their roster.
+          I am honored to be working with them.
+        </p>
+
+      </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-2 col-md-2 col-lg-3">
+    </div>
+
+  </div>
+</div>
